b7f6a58ca965cd7541f73b00bc24698613fb2e13
Ricardo-909-Leal <ricardoeguiff@gmail.com>
2025-05-04 14:20:41 -0300

    classe aluno semi-preparada

--- src/aluno.java
+++ src/aluno.java
@@ -1,16 +1,30 @@
-public class aluno {
+public abstract class Aluno {
     private String nome;
     private String maricula;
     private String nascimento;
 
-    private String presença;//
+ //   private String presença;     ..... colocar como polimorfismo na aparte 3 do menu  
 
-    public aluno() {
-        this.nome = "";
-        this.maricula = "";
-        this.nascimento = "";
+    public Aluno(String nome, String maricula, String nascimento) {
+        this.nome = nome;
+        this.maricula = maricula;
+        this.nascimento = nascimento;
     }
-
+    
+    public String getNome() {
+        return this.nome;
+    }
+    public String getMaricula() {
+        return this.maricula;
+    }
+    public String getNascimento() {
+        return this.nascimento;
+    }
+    public void imprimir() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Matricula: " + maricula); 
+        System.out.println("Nascimento: " + nascimento);
+    }
 
 
 
